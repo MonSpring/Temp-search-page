29ac5f906babf43e9dfa0a2db9fa948afc432741
kimyuntae19 <kimyuntae19@gmail.com>
2022-11-18 05:41:08 +0900

    security ignore custom html + refreshToken DB insert confirm

--- src/main/java/com/example/testsearch/controller/MemberController.java
+++ src/main/java/com/example/testsearch/controller/MemberController.java
@@ -6,6 +6,7 @@ import com.example.testsearch.dto.*;
 import com.example.testsearch.service.MemberService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+@Slf4j
 @Controller
 @RequestMapping("/user")
 @RequiredArgsConstructor
@@ -30,13 +32,11 @@ public class MemberController {
 
     // 로그인 요청 처리
     @PostMapping("/login")
-    public String login(@RequestBody LoginReqDto loginReqDto, Model model) {
-        if (memberService.loginAccount(loginReqDto).getStatusCode().is4xxClientError()) {
-            return "redirect:/user/login";
-        } else {
-            model.addAttribute(memberService.loginAccount(loginReqDto));
-            return "search";
-        }
+    public String loginProc(LoginReqDto loginReqDto, Model model) {
+        log.info("logindto : " + loginReqDto);
+        ResponseEntity<?> responseEntity = memberService.loginAccount(loginReqDto);
+        model.addAttribute(responseEntity);
+        return "search";
     }
 
     // 회원 가입 페이지
